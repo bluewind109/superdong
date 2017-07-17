42a589dd80bc30f496ec0698193a54e73639f6d0
Khan Hoang <khanh.hoangquoc91@gmail.com>
2017-07-18 02:45:03 +0700

    change slider to carousel
    Use Carousel feature of BS instead of using jquery func to implement SLIDER

---
+++ superdong3.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- BASIC INFO (title, keywords, description) -->
+  <title>superdong</title>
+
+  <!-- LIBRARIES CSS -->
+  <link href=".\css\bootstrap.min.css" rel="stylesheet">
+  <link href=".\css\bootstrap-theme.min.css" rel="stylesheet">
+  <link href=".\css\style2.css" rel="stylesheet">
+  
+  <!-- FONTS -->
+ 
+
+</head>
+<body>
+<div class="container">
+  <div class="row"> <!-- HEADER -->
+    <div class="col-sm-1"> <!-- LOGO -->
+      <a class="navbar-logo" href="#"></a>
+    </div>
+
+    <div class="col-sm-11"> <!-- NAVBAR -->
+      <div class="row">
+        <nav class="navbar-default nav-upper navbar-static-top">
+          <div class="container-fluid">
+            <ul class="nav navbar-nav navbar-upper">
+              <li class="nav-item"><a class="active" href="#">Trang chủ</a>
+              </li>
+              <li class="nav-item"><a href="#">Công ty</a>
+              </li>
+              <li class="nav-item"><a href="#">Cẩm nang du lịch</a>
+              </li>
+              <li class="nav-item"><a href="#">Tuyển dụng</a>
+              </li>
+              <li class="nav-item"><a href="#">Hình ảnh</a>
+              </li>
+              <li class="nav-item"><a href="#">Liên hệ</a>
+              </li>
+              <li class="nav-item"><a href="#"><img src="http://superdong.com.vn/wp-content/themes/pl_superdong/img/eng_lg.png" alt="" /></a>
+              </li>
+              <li class="nav-item"><a href="#"><img src="http://superdong.com.vn/wp-content/themes/pl_superdong/img/vi_lg.png" alt="" style="padding-left: 5px;" /></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div> <!--end menu 1 -->
+
+      <div class="row">
+        <nav class="navbar navbar-default navbar-static-top nav-lower">
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbar">
+              <ul class="nav navbar-nav navbar-lower">
+                <li class="nav-item"><a href="#">DỊCH VỤ</a>
+                </li>
+                <li class="nav-item"><a href="#">HỆ THỐNG BÁN VÉ</a>
+                </li>
+                <li class="nav-item"><a href="#">QUAN HỆ CỔ ĐÔNG</a>
+                </li>
+                <li class="nav-item"><a href="#">TIN TỨC</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div> <!--end menu 2 -->
+    </div>  <!-- end NAVBAR -->
+  </div> <!-- end HEADER -->
+
+    <div class="row">
+      <div class="carousel slide" data-ride="carousel" id="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#carousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner">
+
+          <div class="item active">
+            <img src="http://superdong.com.vn/wp-content/uploads/2017/07/banner00-1366x630.jpg" alt ="">
+          </div>
+
+          <div class="item">
+            <img src="http://superdong.com.vn/wp-content/uploads/2017/06/superdong-banner01-2-1366x630.jpg" alt ="">
+          </div>
+
+          <div class="item">
+            <img src="http://superdong.com.vn/wp-content/uploads/2017/06/superdong-banner02-2-1366x630.jpg" alt ="">
+          </div>
+
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="row"> <!-- LOCATION navbar -->
+      <nav class="navbar navbar-default navbar-static-top nav-location">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav navbar-location">
+              <li class="nav-item"><a href="#">Sóc Trăng</a>
+              </li>
+              <li class="nav-item"><a href="#">Côn Đảo</a>
+              </li>
+              <li class="nav-item"><a href="#">Rạch Giá</a>
+              </li>
+              <li class="nav-item"><a href="#">Phú Quốc</a>
+              </li>
+              <li class="nav-item"><a href="#">Nam Du</a>
+              </li>
+              <li class="nav-item"><a href="#">Hà Tiên</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div> <!-- end LOCATION navbar -->
+</div> <!-- end container -->
+
+  <!-- JQUERY-->
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.0.min.js"></script>
+
+  <!--COMPILED AND MINIFIED JAVASCRIPT-->
+  <script src=".\js\bootstrap.min.js"></script>
+
+  <script type='text/javascript'>
+  /* <![CDATA[ */
+  var _wpcf7 = {"loaderUrl":"http:\/\/superdong.com.vn\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+  /* ]]> */
+  </script>
+  <script type='text/javascript' src='http://superdong.com.vn/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=3.8.1'></script>
+  <script type='text/javascript' src='http://superdong.com.vn/wp-includes/js/wp-embed.min.js?ver=4.8'></script>
+
+</body>
+</html>
